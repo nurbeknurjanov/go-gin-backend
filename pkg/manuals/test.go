0cf3004f1c2a7310790aa1925ca1f41fe826c578
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2024-12-08 14:51:19 +0600

    select

--- pkg/manuals/test.go
+++ pkg/manuals/test.go
@@ -37,12 +37,32 @@ type Test struct {
 	fmt.Println("End")
 }*/
 
-func CallMe(x string, y ...any) {
+/*func CallMe(x string, y ...any) {
 	fmt.Println(x)
 	fmt.Println(y...) //only any
 }
-
 func RunTest() {
 	y := []any{1, 2, 3}
 	CallMe("a", y...) //только слайсы
+}*/
+
+func goOne(ch chan string) {
+	ch <- "From goOne goroutine"
+}
+func goTwo(ch chan string) {
+	ch <- "From goTwo goroutine"
+}
+func RunTest() {
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go goOne(ch1)
+	go goTwo(ch2)
+
+	select {
+	case msg11 := <-ch1:
+		fmt.Println("msg11", msg11)
+	case msg22 := <-ch2: //stronger
+		fmt.Println("msg22", msg22)
+	}
 }
